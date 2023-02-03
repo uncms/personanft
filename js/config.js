d9c3e32f3dc7047dc8bdbc3272e0d770e4c93a8b
UNCOMMONS <118422255+uncms@users.noreply.github.com>
2023-02-03 16:25:24 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,3 +1,176 @@
-const CONTRACTADDRESS = "";
+const CONTRACTADDRESS = "0x26271af0B02a846d0c7792F2d8e960080Ff8dD1C";
 
-const ABI = [];
+const ABI = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "canWriteComment",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_projectID",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_commentID",
+        type: "uint256",
+      },
+    ],
+    name: "deleteComment",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_projectID",
+        type: "uint256",
+      },
+    ],
+    name: "getAllData",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+      {
+        internalType: "string[]",
+        name: "",
+        type: "string[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_projectID",
+        type: "uint256",
+      },
+    ],
+    name: "getCommentCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_maxProjectID",
+        type: "uint256",
+      },
+    ],
+    name: "setMaxProjectID",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_projectID",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_comment",
+        type: "string",
+      },
+    ],
+    name: "writeComment",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
